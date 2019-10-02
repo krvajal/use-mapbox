74c6efbae0d7fddd7bb93558c11362b9b92a6da1
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2019-10-02 20:03:45 +0200

    feat: add initial implementation

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,60 @@
 import * as React from 'react';
+import mapboxgl, { Map } from 'mapbox-gl';
+import { useSubscription } from 'use-subscription';
 
-export default function useMapbox(containerId: string, config = {}) {
-  const mapRef = React.useRef(null);
-  return { map: mapRef.current };
+type Maybe<T> = T | null;
+
+export default function useMapbox(containerId: string, config: any = {}) {
+  const { accessToken, ...options } = config;
+  const mapRef = React.useRef<Maybe<Map>>(null);
+  const [initialized, setInitialized] = React.useState(false);
+
+  React.useEffect(() => {
+    mapRef.current = new mapboxgl.Map({
+      container: containerId,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      accessToken,
+      ...options,
+    });
+
+    setInitialized(true);
+
+    return () => {
+      if (mapRef.current) {
+        mapRef.current!.remove();
+        mapRef.current = null;
+        setInitialized(false);
+      }
+    };
+  }, []);
+
+  const loaded = useSubscription(
+    React.useMemo(
+      () => ({
+        getCurrentValue() {
+          return mapRef.current ? mapRef.current!.loaded() : false;
+        },
+        subscribe: callback => {
+          if (mapRef.current) {
+            mapRef.current!.on('load', callback);
+          }
+
+          return () => {
+            if (mapRef.current) {
+              mapRef.current!.off('load', callback);
+            }
+          };
+        },
+      }),
+      [initialized]
+    )
+  );
+
+  React.useEffect(() => {
+    if (loaded && mapRef.current) {
+      mapRef.current.addControl(new mapboxgl.NavigationControl(), 'top-right');
+    }
+  }, [loaded]);
+
+  return { loaded, map: mapRef.current };
 }
